d74e43b2e083c5a064cffbe2da5dba2cd889db85
Filip Rudek <wojciechrudek@gmail.com>
2024-03-05 22:38:19 +0100

    fixed checkUserRole, created renderResetPasswordCopy func

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,5 +1,5 @@
-import { confirmPasswordReset, getAuth } from 'firebase/auth';
-import { collection, getDoc, where } from 'firebase/firestore';
+import { confirmPasswordReset, getAuth, verifyPasswordResetCode } from 'firebase/auth';
+import { collection, getDocs, query, where } from 'firebase/firestore';
 import { db } from '../firebase';
 
 export const handleClick = (event, sectionRefs, sectionId) => {
@@ -17,14 +17,29 @@ export const confirmThePasswordReset = async (oobCode, newPassword) => {
 export const checkUserRole = async (oobCode) => {
     if (!oobCode) return;
     try {
+        const auth = getAuth();
+        const email = await verifyPasswordResetCode(auth, oobCode);
         const collectionRef = collection(db, 'passwordResetRequests');
-        const query = await getDoc(collectionRef, where('oobCode', '==', oobCode));
-        const data = query.data();
-        console.log(data.role);
-
-        return data.role;
+        const docQuery = query(collectionRef, where('email', '==', email));
+        const docSnap = await getDocs(docQuery);
+        if (!docSnap.empty) {
+            return docSnap.docs[0].data().role;
+        } else {
+            console.log('No document found for the given email');
+        }
     } catch (error) {
         console.error('Error getting user:', error);
         throw error;
+    }
+};
+
+export const renderResetPasswordCopy = (userRole) => {
+    switch (userRole) {
+        case 'user':
+            return 'Wpisz nowe hasło dla konta użytkownika';
+        case 'admin':
+            return 'Wpisz nowe hasło dla admina';
+        default:
+            return 'Wygląda na to, że twój link wygasł. Spróbuj ponownie.';
     }
 };
